9c875c13c07c332341bc541eaa830668edf67454
fightypants <kha.les.email@gmail.com>
2015-05-01 16:00:58 -0700

    Will add WIP erb files

--- app.rb
+++ app.rb
@@ -5,9 +5,18 @@ require('./lib/definition')
 require('./lib/word')
 
 get('/') do
+  @words = Word.all()
   erb(:index)
 end
 
-get('/new_word')
-  erb(:new_word)
+get('/new_word') do
+   erb(:new_word)
 end
+
+post('/word_definition') do
+  @word = params.fetch('word')
+  @definition = params.fetch('definition')
+  erb(:word_definition)
+end
+
+#need logic here to push word and definition into server
